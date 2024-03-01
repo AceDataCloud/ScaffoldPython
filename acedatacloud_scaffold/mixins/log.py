484a476816ab072b79a6c2eac4bc4861fcbbaaed
Germey <cqc@cuiqingcai.com>
2024-03-02 00:55:07 +0800

    add extra arg

--- acedatacloud_scaffold/mixins/log.py
+++ acedatacloud_scaffold/mixins/log.py
@@ -3,14 +3,14 @@ from loguru import logger
 
 class LogMixin(object):
 
-    def log(self, level, message):
+    def log(self, level, message, *args, **kwargs):
         """
         Custom log method that includes the trace ID.
         """
         if isinstance(message, bytes):
             message = message.decode('utf-8')
         trace_message = f"[{self.trace_id}] {message}"
-        getattr(logger, level)(trace_message)
+        getattr(logger, level)(trace_message, *args, **kwargs)
 
     @property
     def logger(self):
@@ -21,22 +21,22 @@ class LogMixin(object):
             def __init__(self, outer):
                 self.outer = outer
 
-            def debug(self, message):
-                self.outer.log('debug', message)
+            def debug(self, message, *args, **kwargs):
+                self.outer.log('debug', message, *args, **kwargs)
 
-            def info(self, message):
-                self.outer.log('info', message)
+            def info(self, message, *args, **kwargs):
+                self.outer.log('info', message, *args, **kwargs)
 
-            def warning(self, message):
-                self.outer.log('warning', message)
+            def warning(self, message, *args, **kwargs):
+                self.outer.log('warning', message, *args, **kwargs)
 
-            def error(self, message):
-                self.outer.log('error', message)
+            def error(self, message, *args, **kwargs):
+                self.outer.log('error', message, *args, **kwargs)
 
-            def critical(self, message):
-                self.outer.log('critical', message)
+            def critical(self, message, *args, **kwargs):
+                self.outer.log('critical', message, *args, **kwargs)
 
-            def exception(self, message):
-                self.outer.log('exception', message)
+            def exception(self, message, *args, **kwargs):
+                self.outer.log('exception', message, *args, **kwargs)
 
         return LoggerAdapter(self)
